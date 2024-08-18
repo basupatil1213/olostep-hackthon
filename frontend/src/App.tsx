0a8a72096e52d90ddbd1cccda63953b64e10b20d
basupatil1213 <patil.ba@northeastern.edu>
2024-08-18 05:00:07 -0400

    set base url to localhost

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes/Routes.tsx";
 import NavBar from "./components/NavBar.tsx";
 
-export const BASE_URL = "https://webscraping-app-615396515843.us-east1.run.app";
-
+export const BASE_URL = "http://localhost:5000";
+// https://webscraping-app-615396515843.us-east1.run.app
 function App() {
     return (
         <Router>
